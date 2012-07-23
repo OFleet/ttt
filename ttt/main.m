37c5dfef783690e09010d11672a15bf562a2d5ea
RomanSforza <roman.vmelnikov@gmail.com>
2012-07-23 15:09:15 +1100

    Revert "Edit main.m"
    This reverts commit 6ad1b20948a3c0265ffa5c5604444c07b4d3a5d1.

--- ttt/main.m
+++ ttt/main.m
@@ -3,7 +3,7 @@
 //  ttt
 //
 //  Created by Roman Sforza on 7/10/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights resvvverved.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
